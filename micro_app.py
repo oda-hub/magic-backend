1c2dd353c4796548b17e7fee584457c2cdfe2ce9
andrea tramacere <orion@MacBook-Pro-3.local>
2019-09-25 12:22:00 +0200

    improving catalog

--- micro_app.py
+++ micro_app.py
@@ -91,7 +91,7 @@ def get_data():
         _o_dict = json.dumps(_o_dict)
     except Exception as e:
         raise APIerror('table file is empty/corrupted or missing', status_code=410)
-
+        raise RuntimeError
     #_o_dict = json.loads(_o_dict)
     #t_rec = base64.b64decode(_o_dict['table'])
     #t_rec = pickle.loads(t_rec)
