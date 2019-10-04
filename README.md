9503f06eef668c7d1fe3031f1e26f6b5d2768c16
andrea tramacere <orion@MacBook-Pro-3.local>
2019-10-04 12:53:29 +0200

    added flask_restplus

---
+++ README.md
@@ -0,0 +1,37 @@
+MAGIC backend API
+==========================================
+*API for MAGIC backend*
+
+What's the license?
+-------------------
+
+magic-backend is distributed under the terms of The MIT License.
+
+Who's responsible?
+-------------------
+Andrea Tramacere
+
+ISDC Data Centre for Astrophysics, Astronomy Department of the University of Geneva, Chemin d'Ecogia 16, CH-1290 Versoix, Switzerland
+
+
+Installation
+-------------------
+1) Anaconda
+    * `conda config --add channels conda-forge`
+    * `conda --file requirements.txt`
+    * `python setup.py install`
+    
+2) PIP
+    * `pip install -r requirements.txt`
+    * `python setup.py install`
+
+Documentation
+-------------------
+A) For local development  set the `PORT` in the config.py to '5000'
+
+1) run the app: `python micro_app.py`
+2) browse this url to get api doc `http://localhost:5000/`
+3) open the `magic-test.ipynb` notebook
+
+B) For  deployment: 
+1) set the `PORT` in the config.py to the valued used on that machine
--- micro_app.py
+++ micro_app.py
@@ -6,17 +6,76 @@ from builtins import (open, str, range,
 
 from oda_api.data_products import AstropyTable
 from flask import Flask, jsonify, abort,request
+from flask_restplus import Api, Resource,reqparse
+
 from astropy.table import Table
 from flask.json import JSONEncoder
 import numpy as np
 import json
 import yaml
-import sys
-import inspect
+import pickle
+import base64
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 
 
 micro_service = Flask("micro_service")
+api= Api(app=micro_service, version='1.0', title='MAGIC back-end API',
+    description='API to extract data for MAGIC Telescope\n Author: Andrea Tramacere',)
+ns_conf = api.namespace('api/v1.0/magic', description='Conference operations')
+
+parser = reqparse.RequestParser()
+parser.add_argument('file_name')
+
+class AstropyTable(object):
+
+    def __init__(self,table_object,name='astropy table', meta_data={}):
+        self.name=name
+        self.meta_data=meta_data
+        self.table=table_object
+
+    def encode(self,use_binary=False,to_json = False):
+
+        _o_dict = {}
+        _o_dict['binary']=None
+        _o_dict['ascii']=None
+
+        if use_binary is True:
+            _binarys = base64.b64encode(pickle.dumps(self.table, protocol=2)).decode('utf-8')
+            _o_dict['binary'] = _binarys
+        else:
+            fh=StringIO()
+            self.table.write(fh, format='ascii.ecsv')
+            _text = fh.getvalue()
+            fh.close()
+            _o_dict['ascii'] = _text
+
+        _o_dict['name']=self.name
+        _o_dict['meta_data']=json.dumps(self.meta_data)
+
+        if to_json == True:
+            _o_dict=json.dumps(_o_dict)
+        return   _o_dict
+
+    @classmethod
+    def decode(cls,_o_dict,use_binary=False):
+
+        encoded_name = _o_dict['name']
+        encoded_meta_data = _o_dict['meta_data']
+        if use_binary is True:
+            t_rec = base64.b64decode(_o_dict['binary'])
+            try:
+                t_rec = pickle.loads(t_rec)
+            except:
+                t_rec= pickle.loads(t_rec,encoding='latin')
+
+        else:
+            t_rec = ascii.read(_o_dict['ascii'])
+
+        return cls(t_rec,name=encoded_name,meta_data=encoded_meta_data)
 
 
 
@@ -54,57 +113,54 @@ def handle_api_error(error):
     response.status_code = error.status_code
     return response
 
+@ns_conf.route('/catalog')
+@api.doc()
+class Catalog(Resource):
+    @api.doc(responses={410: 'catalog file is empty/corrupted or missing'})
+    def get(self):
+        """
+        returns the catalog
+        """
+        try:
+            with open('MAGIC_data/data/19e/magic_19e.yaml') as f:
+                data = yaml.load(f)
 
-@micro_service.route('/api/v1.0/magic/get-catalog', methods=['GET','POST'])
-def get_catalog():
+            _o_dict = json.dumps(data,sort_keys=False)
+            print(_o_dict)
+            return jsonify(_o_dict)
+        except Exception as e:
+            raise APIerror('table file is empty/corrupted or missing', status_code=410)
 
-    with open('MAGIC_data/data/19e/magic_19e.yaml') as f:
-        data = yaml.load(f)
+@ns_conf.route('/data')
+@api.doc(responses={410: 'table file is empty/corrupted or missing'}, params={'file_name': 'the file name'})
+class Data(Resource):
+    def get(self):
+        """
+        returns the astropy table
+        """
 
-    _o_dict = json.dumps(data,sort_keys=False)
-    print(_o_dict)
-    return jsonify(_o_dict)
 
 
-@micro_service.route('/api/v1.0/magic/get-data', methods=['GET','POST'])
-def get_data():
-    if request.method == 'GET':
-        args = request.args
-    if request.method == 'POST':
-        args = request.form
+        try:
+            args = parser.parse_args()
+            file_name = args['file_name']
+            print('file_name', file_name)
+            t = AstropyTable(Table.read('MAGIC_data/data/19e/%s'%file_name, format='ascii'),name='MAGIC TABLE')
+            _o_dict = {}
+            _o_dict['astropy_table'] = t.encode(use_binary=False)
+            _o_dict = json.dumps(_o_dict)
+        except Exception as e:
+            raise APIerror('table file is empty/corrupted or missing', status_code=410)
 
-    par_dic = args.to_dict()
-    file_name=par_dic['file_name']
 
-    #with open("MAGIC_data/19e/magic_19e_sed_fig3_mwl_target01.ecsv") as read_file:
-    #    table_text = read_file.read()
-    #out_dict={}
-    #out_dict['products']={}
-    #out_dict['products']['astropy_table_product_list'] = [json.dumps(table_text)]
-    # print ( 'ECCO',out_dict['products']['numpy_data_product_list'],_p,_npdl)
-    print ('file_name',file_name)
-    try:
-        t = AstropyTable(Table.read('MAGIC_data/data/19e/%s'%file_name, format='ascii'),name='MAGIC TABLE')
 
-        _o_dict = {}
-        _o_dict['astropy_table'] = t.encode(use_binary=False)
-        _o_dict = json.dumps(_o_dict)
-    except Exception as e:
-        raise APIerror('table file is empty/corrupted or missing', status_code=410)
+        return jsonify(_o_dict)
 
-    #_o_dict = json.loads(_o_dict)
-    #t_rec = base64.b64decode(_o_dict['table'])
-    #t_rec = pickle.loads(t_rec)
-    #t_rec
 
-    return jsonify(_o_dict)
-
-#def run_micro_service(conf,debug=False,threaded=False):
-#    micro_service.config['conf'] = conf
-#    #if conf.sentry_url is not None:
-#    print('conf micro',micro_service.config['conf'])
-#    #sentry = Sentry(app, dsn=conf.sentry_url)
-#    micro_service.run(host=conf.microservice_url, port=conf.microservice_port, debug=debug,threaded=threaded)
+#api.add_resource(Catalog, '/api/v1.0/magic/get-catalog')
+#api.add_resource(Data, '/api/v1.0/magic/get-data')
 
 if __name__ == '__main__':
-    micro_service.run(host="0.0.0.0", port=32792)
+    micro_service.config.from_pyfile('config.py')
+    print(micro_service.config)
+    micro_service.run(host="0.0.0.0",port=micro_service.config['PORT'])
